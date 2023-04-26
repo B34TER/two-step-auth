6b4b6118e9645946bb7f08b8b86c6d9f73435503
B34TER <sew.szymanski@gmail.com>
2023-04-26 23:37:24 +0200

    Remove spelling mistake from cors

--- backend/config/constants.js
+++ backend/config/constants.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   `http://localhost:5173`,
-  "https://twostepauth.vercel.app/",
-  "https://two-step-auth.vercel.app/",
+  "https://twostepauth.vercel.app",
+  "https://two-step-auth.vercel.app",
 ];
 
 module.exports = {
